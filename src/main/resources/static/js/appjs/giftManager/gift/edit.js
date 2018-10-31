9848f69cfcd487c59b1b1765623c2e193ee8df07
weiliangzhou <382308664@qq.com>
2018-10-31 10:14:32 +0800

    测试看看

---
+++ src/main/resources/static/js/appjs/giftManager/gift/edit.js
@@ -0,0 +1,166 @@
+$().ready(function () {
+    validateRule();
+});
+
+$.validator.setDefaults({
+    submitHandler: function () {
+        update();
+    }
+});
+
+function update() {
+    $.ajax({
+        cache: true,
+        type: "POST",
+        url: "/gift/update",
+        data: $('#signupForm').serialize(),// 你的formid
+        async: false,
+        error: function (request) {
+            parent.layer.alert("Connection error");
+        },
+        success: function (data) {
+            if (data.code == 0) {
+                parent.layer.msg("操作成功");
+                parent.reLoad();
+                var index = parent.layer.getFrameIndex(window.name); // 获取窗口索引
+                parent.layer.close(index);
+
+            } else {
+                parent.layer.alert(data.msg)
+            }
+
+        }
+    });
+
+}
+
+function validateRule() {
+    var icon = "<i class='fa fa-times-circle'></i> ";
+    $("#signupForm").validate({
+        rules: {
+            giftMainTitle: {
+                required: true
+            },
+            giftViceTitle: {
+                required: true
+            }
+
+
+        },
+        messages: {
+            giftMainTitle: {
+                required: icon + "请输入主标题"
+            },
+            giftViceTitle: {
+                required: icon + "请输入副标题"
+            }
+        }
+    });
+}
+
+layui.use('upload', function () {
+    var $ = layui.jquery, upload = layui.upload;
+    //上传图片
+    upload.render({
+        elem: '#giftMainImgBtn' //绑定元素
+        , url: '/file/upload/' //上传接口
+        , accept: 'images'
+        , before: function (obj) {
+            //预读本地文件示例，不支持ie8
+            obj.preview(function (index, file, result) {
+                $('#giftMainImgShow').attr('src', result); //图片链接（base64）
+            });
+        }
+        , done: function (res) {
+            if (res.code == 0) {
+                parent.layer.msg("上传成功");
+                $('#giftMainImg').val(res.data.src);
+            }
+            //上传完毕回调
+        }
+        , error: function () {
+            //请求异常回调
+        }
+    });
+    //上传图片
+    upload.render({
+        elem: '#giftViceImg1Btn' //绑定元素
+        , url: '/file/upload/' //上传接口
+        , accept: 'images'
+        , before: function (obj) {
+            //预读本地文件示例，不支持ie8
+            obj.preview(function (index, file, result) {
+                $('#giftViceImg1Show').attr('src', result); //图片链接（base64）
+            });
+        }
+        , done: function (res) {
+            if (res.code == 0) {
+                parent.layer.msg("上传成功");
+                $('#giftViceImg1').val(res.data.src);
+            }
+            //上传完毕回调
+        }
+        , error: function () {
+            //请求异常回调
+        }
+    });
+    //上传图片
+    upload.render({
+        elem: '#giftViceImg2Btn' //绑定元素
+        , url: '/file/upload/' //上传接口
+        , accept: 'images'
+        , before: function (obj) {
+            //预读本地文件示例，不支持ie8
+            obj.preview(function (index, file, result) {
+                $('#giftViceImg2Show').attr('src', result); //图片链接（base64）
+            });
+        }
+        , done: function (res) {
+            if (res.code == 0) {
+                parent.layer.msg("上传成功");
+                $('#giftViceImg2').val(res.data.src);
+            }
+            //上传完毕回调
+        }
+        , error: function () {
+            //请求异常回调
+        }
+    });
+    //上传图片
+    upload.render({
+        elem: '#giftViceImg3Btn' //绑定元素
+        , url: '/file/upload/' //上传接口
+        , accept: 'images'
+        , before: function (obj) {
+            //预读本地文件示例，不支持ie8
+            obj.preview(function (index, file, result) {
+                $('#giftViceImg3Show').attr('src', result); //图片链接（base64）
+            });
+        }
+        , done: function (res) {
+            if (res.code == 0) {
+                parent.layer.msg("上传成功");
+                $('#giftViceImg3').val(res.data.src);
+            }
+            //上传完毕回调
+        }
+        , error: function () {
+            //请求异常回调
+        }
+    });
+
+
+});
+
+layui.use('layedit', function () {
+    var layedit = layui.layedit;
+    layedit.set({
+        uploadImage: {
+            url: '/file/upload/' //接口url
+            , type: 'post' //默认post
+        }
+    });
+
+    var giftDesc = layedit.build('giftDesc'); //建立编辑器
+});
+
